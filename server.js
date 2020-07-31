0520d29584c7c39c884fcdbb2dcf3408c94fa9ab
bryantobing12 <bryantobing0@gmail.com>
2020-07-31 20:16:31 +0700

    make directory structure

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('./db/mongoose');
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+const publicDirectoryPath = path.join(__dirname, '/public');
 
-app.listen(3000, function () {
-    console.log("server jalan");
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, function () {
+    console.log(`Server started on ${port}`);
 });
