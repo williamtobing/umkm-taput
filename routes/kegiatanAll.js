2fa9956606b1c74ac55862e22239a2c69819be71
bryantobing12 <bryantobing0@gmail.com>
2020-08-03 01:13:54 +0700

    kegiatan add feature done

--- routes/kegiatanAll.js
+++ routes/kegiatanAll.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment-timezone');
 const router = new express.Router();
 
 const Kegiatan = require('./../models/kegiatan');
@@ -6,9 +7,28 @@ const Kegiatan = require('./../models/kegiatan');
 router.get('/kegiatan', async (req, res) => {
     try {
         const kegiatan = await Kegiatan.find({}).sort({createdAt : 'desc'});
+        kegiatan.forEach(keg => {
+            keg.tanggalDitambahkan = moment(keg.createdAt).tz('Asia/Jakarta').locale('id').format('LL');
+        });
         res.render('kegiatan', {
             title : "Kegiatan",
             listData : kegiatan
+        })
+    } catch(e) {
+        res.json({
+            message : e.message
+        })
+    }
+});
+
+router.get('/kegiatan/detail/:id', async (req, res) => {
+    try {
+        const kegiatan = await Kegiatan.findById(req.params.id);
+        kegiatan.tanggalDitambahkan = moment(kegiatan.createdAt).tz('Asia/Jakarta').locale('id').format('LL');
+        kegiatan.formatted = kegiatan.deskripsiKegiatan.split('\n');
+        res.render('detail-kegiatan', {
+            title : "Detail Kegiatan",
+            data : kegiatan
         })
     } catch(e) {
         res.json({
